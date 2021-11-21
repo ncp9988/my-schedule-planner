1045480d63bdd975f982495ac9fce06b45c0379c
ncp9988@gmail.com <v.i.c@Chaus-MBP.hitronhub.home>
2021-11-21 12:33:29 -0600

    color-coded

--- assets/script.js
+++ assets/script.js
@@ -3,34 +3,28 @@ $('#currentDay').text(moment().format('LLLL'))
 
 //time block is color-coded
 let now = moment().hour(); 
-let textEntry ="#text-entry"
-let time = "#time";
-let timeString =$(time).text();
-let timeBlock = switchHours(timeString);
-if (timeBlock < now) {
-    $(textEntry).addClass("past");
-} else if (timeBlock > now) {
-    $(textEntry).addClass("future")
-} else {
-    $(textEntry).addClass("present")
-}
 
-//switch time to 24 hours clock
-function switchHours(timeString) {
-switch(timeString) {
-    case "8AM" : return 8 ; 
-    case "9AM" : return 9 ;
-    case "10AM" : return 10 ;
-    case "11AM" : return 11 ;
-    case "12AM" : return 12 ;
-    case "1PM" : return 13 ;     
-    case "2PM" : return 14 ;
-    case "3PM" : return 15 ;
-    case "4PM" : return 16 ;
-    case "5PM" : return 17 ;
+// let textEntry ="#text-entry"
+// let time = "#time";
+// let timeString =$(time).text();
+// let timeBlock = switchHours(timeString);
 
-}
-}
+// //switch time to 24 hours clock
+// function switchHours(timeString) {
+// switch(timeString) {
+//     case "8AM" : return 8 ; 
+//     case "9AM" : return 9 ;
+//     case "10AM" : return 10 ;
+//     case "11AM" : return 11 ;
+//     case "12AM" : return 12 ;
+//     case "1PM" : return 13 ;     
+//     case "2PM" : return 14 ;
+//     case "3PM" : return 15 ;
+//     case "4PM" : return 16 ;
+//     case "5PM" : return 17 ;
+
+// }
+// }
 // var textEl = document.querySelector('#text-entry')
 // let schedule = {
 //     "9AM": textEl.value,
@@ -51,6 +45,15 @@ $("button").click(function(){
 for (let i = 9; i<= 17; i++) {
     var timeEntry = localStorage.getItem(i)
     $("#time-"+i).siblings("textarea").val(timeEntry)
+
+    if (i < now) {
+        $("#time-"+i).siblings("textarea").addClass("past");
+    } else if (i > now) {
+        $("#time-"+i).siblings("textarea").addClass("future")
+    } else {
+        $("#time-"+i).siblings("textarea").addClass("present")
+    }
+    
 }
 
 
